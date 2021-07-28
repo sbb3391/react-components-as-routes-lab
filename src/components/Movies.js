27c2d97dadc0b5c5dfb5ff6dc9cbde595cde2331
Sam Bequette <sbb3391@gmail.com>
2021-07-28 19:11:47 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { movies } from '../data';
 
+
 const Movies = () => {
+
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie, index) => (
+          <div key={index}>
+            <h1>{movie.title}</h1>
+            <h2>{movie.time}</h2>
+            <ul>
+              {movie.genres.map((genre, index) => (
+                <ul key={index}>{genre}</ul>
+              ))}
+            </ul>
+          </div>
+        ))
+        }
+
     </div>
   );
 };
